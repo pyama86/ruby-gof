f18a2810662d0403e9ea9c23aed09307672f63f0
kazuhiko yamahsita <pyama@pepabo.com>
2015-08-28 00:26:07 +0900

    update base

--- base/maze.rb
+++ base/maze.rb
@@ -1,8 +1,10 @@
 class Maze
   def initialize; end
   def add_room(room)
-    @room ||= []
-    @room << room
+    @rooms ||= []
+    @rooms << room
   end
-  def room_no(no); end
+  def room_no(no)
+    @rooms.find {|room| room.room_no == no } if @rooms
+  end
 end
